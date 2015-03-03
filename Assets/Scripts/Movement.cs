7c8df25e76eed809d8666f62c373fabc28ab31f0
Bryce Billing <bllbry001@myuct.ac.za>
2015-03-03 14:28:27 +0200

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate(new Vector3(-speed,0,0)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(new Vector3(0,speed,0)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(new Vector3(0,-speed,0)*Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate(new Vector3(speed,0,0)*Time.deltaTime);
+		}
+	}
+}
