a8516eafa9eedc9f97bfab8ce1242ed5d77d6c38
Bryce Billing <bllbry001@myuct.ac.za>
2015-03-06 00:09:46 +0200

    Simple AI created

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed;
+	public Transform player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float direction = Mathf.Atan2 ((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+		transform.eulerAngles = new Vector3 (0,0,direction);
+		rigidbody2D.AddForce (gameObject.transform.up*speed);
+	}
+}
