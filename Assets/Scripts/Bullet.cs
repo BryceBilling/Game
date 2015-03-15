9d2f3c3ce1f3443c68852379278824e392d696d6
Bryce Billing <bllbry001@myuct.ac.za>
2015-03-15 02:03:51 +0200

    Added player shooting and simpler npc ai

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Die", 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Die () {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Die",1.8f);
+	}
+	
+	// Update is called once per frame
+	void Die () {
+		Destroy (gameObject);
+	}
+}
